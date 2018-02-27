12be1b62da34f9881f8c8cbbf576397ab3524f65
yasuko_sekine <kuronene23@gmail.com>
2018-02-27 09:32:52 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -13,8 +13,20 @@
   		
   		<div class="collapse navbar-collapse" id="bs-example-navber-cllapse-1">
   			<ul class="nav navbar-nav navbar-right">
-  				<li>{!! link_to_route('signup.get', 'Signup') !!}</li>
-  				<li><a href="#">Login</a></li>
+  			  @if (Auth::check())
+    			  <li><a href="#">Users</a></li>
+    			  <li class="dropdown">
+    			    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">My profile</a></li>
+                <li role="separator" class="divider"></li>
+                <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
+              </ul>
+    			  </li>
+  			   @else
+    				<li>{!! link_to_route('signup.get', 'Signup') !!}</li>
+    				<li>{!! link_to_route('login.get', 'Login') !!}</li>
+    			 @endif
   			</ul>
   		</div>
   	</div>
